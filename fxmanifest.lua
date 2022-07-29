52f4afa038740a5c648b0aed4770acc66b708494
CJGuirao <carlos.jimenez.guirao@gmail.com>
2022-07-29 20:31:11 +0000

    Mod to upload a copy of the messages to a table to be processed later

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,4 @@
+
 fx_version 'cerulean'
 game 'gta5'
 
@@ -5,6 +6,7 @@ description 'QB-Logs'
 version '1.0.0'
 
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'server/server.lua',
     'config.lua'
 }
--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,18 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone)        
+-- We check for the log table and create it if not exists
+CreateThread(function()
+local tablecheck = exports.oxmysql:executeSync('SHOW TABLES LIKE @logsraw',{['@logsraw'] = 'logsraw',})
+  if tablecheck[1] ~= 'logsraw' then
+     exports.oxmysql:execute("CREATE TABLE IF NOT EXISTS `logsraw` ( `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci', `title` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci', `color` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci', `message` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci', `tageveryone` TINYINT(1) NULL DEFAULT '0', `parsed` TINYINT(1) NOT NULL DEFAULT '0', `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`) USING BTREE, INDEX `created_at` (`created_at`) USING BTREE, INDEX `name` (`name`) USING BTREE, INDEX `tageveryone` (`tageveryone`) USING BTREE , INDEX `parsed` (`parsed`)  )    COLLATE='utf8mb4_0900_ai_ci'    ENGINE=InnoDB    ",{})
+  end
+end)
+
+RegisterNetEvent('qb-log:server:CreateLog', function(name, title, color, message, tagEveryone) 
     local tag = tagEveryone or false
     local webHook = Config.Webhooks[name] or Config.Webhooks['default']
     local embedData = {
-        {
+ {
             ['title'] = title,
             ['color'] = Config.Colors[color] or Config.Colors['default'],
             ['footer'] = {
@@ -17,6 +25,10 @@ RegisterNetEvent('qb-log:server:CreateLog', function(name, title, color, message
             },
         }
     }
+    -- Insert into Database for later use
+    exports.oxmysql:execute('INSERT INTO logsraw (name ,title, color ,message, tageveryone) VALUES (@name ,@title, @color ,@message, @tageveryone)',
+    {['@name'] = name ,['@title'] = title, ['@color'] = color ,['@message'] = message, ['@tageveryone'] = tageveryone, })
+    -- Send to discord webhook
     PerformHttpRequest(webHook, function(err, text, headers) end, 'POST', json.encode({ username = 'QB Logs', embeds = embedData}), { ['Content-Type'] = 'application/json' })
     Citizen.Wait(100)
     if tag then
